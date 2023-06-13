4b51f914432979333a577eafcd74394d09a77a99
JBWERE-coder <lutherwere@gmail.com>
2023-06-13 10:37:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+const num1 = 31, num2 = 2;
+
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+
+
+const num3 = 19, num4 = 5;
+const mod = num3 % num4;
+
+console.log(mod);
+
+let max = Math.max(5, 10, 20);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
